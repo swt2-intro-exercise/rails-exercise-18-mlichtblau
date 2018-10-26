24eeb7e0fee5eb8571757bb3439a148c3eace535
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-10-26 08:37:08 +0200

    Test new author form elements

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,12 @@ describe "New author page", type: :feature do
  it "should render withour error" do
    visit new_author_path
  end
+
+  it "should have input fields for an author first name, last name and homepage" do
+    visit new_author_path
+
+    expect(page).to have_field('author[firstName]')
+    expect(page).to have_field('author[lastName]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
