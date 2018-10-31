ac8eb73ba8e7952b243e0ed5f7c35cb696218550
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-10-31 21:10:58 +0100

    sorry about that

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -4,7 +4,7 @@ describe "Author Index page", type: :feature do
 
   it "should render without errors" do
     @alan = FactoryBot.create :author
-    visit author_index_path
+    visit authors_path
     expect(page).to have_link 'New', href: new_author_path
     expect(page).to have_text @alan.first_name
   end
