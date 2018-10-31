62d053fa14e35432f3a71c6ace63122dc270f134
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-10-31 20:45:12 +0100

    Test author index page

---
+++ spec/features/author/author_index_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Author Index page", type: :feature do
+
+  it "should render without errors" do
+    @alan = FactoryBot.create :author
+    visit author_index_path
+    expect(page).to have_link 'New', href: new_author_path
+    expect(page).to have_text @author.first_name
+  end
+end
