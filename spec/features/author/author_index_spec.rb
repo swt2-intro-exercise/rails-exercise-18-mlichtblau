ec52b019bb9700534165910e3fc0c4f77bcc8f2c
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-10-31 20:50:52 +0100

    Update test

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -6,6 +6,6 @@ describe "Author Index page", type: :feature do
     @alan = FactoryBot.create :author
     visit author_index_path
     expect(page).to have_link 'New', href: new_author_path
-    expect(page).to have_text @author.first_name
+    expect(page).to have_text @alan.first_name
   end
 end
