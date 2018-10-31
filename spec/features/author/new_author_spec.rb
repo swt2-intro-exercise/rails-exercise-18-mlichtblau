a14ac88085cc29da08eab6128ccf225b7ad5ad59
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-10-31 20:06:05 +0100

    Test that new author form should be submittable

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "New author page", type: :feature do
 
- it "should render withour error" do
-   visit new_author_path
- end
+   it "should render withour error" do
+     visit new_author_path
+   end
 
   it "should have input fields for an author first name, last name and homepage" do
     visit new_author_path
@@ -12,5 +12,15 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it "should correctly submit the new author form" do
+    visit new_author_path
+
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+
+    find('input[type="submit"]').click
   end
 end
