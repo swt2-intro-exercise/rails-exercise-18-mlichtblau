e5894dc4dbbaf50ae11dd4a0855bd1d625b47c6a
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-01 09:31:26 +0100

    Test if error shows when creating invalid author

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -23,4 +23,14 @@ describe "New author page", type: :feature do
 
     find('input[type="submit"]').click
   end
+
+  it "should show an error when creating invalid author" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+
+    find('input[type="submit"]').click
+
+    expect(page).to have_text('Last name can\'t be blank')
+  end
 end
