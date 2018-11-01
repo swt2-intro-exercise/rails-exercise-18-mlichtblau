b3938510c08c8331bfaeb5c7fe03d2951a3bce67
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-01 09:13:38 +0100

    Test last_name validation for author

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,16 +3,21 @@ require 'rails_helper'
 describe "Author model", type: :model do
 
   before :each do
-    @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+    @author = FactoryBot.create :author
   end
 
   it "should create new Author with first name, last name and homepage" do
     expect(@author.first_name).to eq("Alan")
     expect(@author.last_name).to eq("Turing")
-    expect(@author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    expect(@author.homepage).to eq("http://wikipedia.de/Alan_Turing")
   end
 
   it "should have a name which combines first name and last name" do
     expect(@author.name).to eq("Alan Turing")
+  end
+
+  it "should not validate when last_name is missing" do
+    @alan = Author.new(first_name: 'Alan', homepage: 'http://wikipedia.de/Alan_Turing')
+    expect(@alan).to_not be_valid
   end
 end
