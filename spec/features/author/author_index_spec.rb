8b0eb1e606e3fadbcba13c7ea05cd14baabb7b57
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-01 18:42:37 +0100

    Test if index page has delete links

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -2,16 +2,22 @@ require 'rails_helper'
 
 describe "Author Index page", type: :feature do
 
-  it "should render without errors" do
+  before :each do
     @alan = FactoryBot.create :author
     visit authors_path
+  end
+
+  it "should render without errors" do
     expect(page).to have_link 'New', href: new_author_path
     expect(page).to have_text @alan.first_name
   end
 
   it "should have a link to the authors edit page" do
-    @alan = FactoryBot.create :author
-    visit authors_path
     expect(page).to have_link href: edit_author_path(@alan)
   end
+
+  it "should have a link to delete the author" do
+    expect(page).to have_link 'Delete'
+  end
+
 end
