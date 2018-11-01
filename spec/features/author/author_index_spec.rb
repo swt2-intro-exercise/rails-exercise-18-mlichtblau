c177c45a9d0d2e993e28955b7c773744cccc9e31
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-01 11:13:31 +0100

    Authors can be changed from index page

--- spec/features/author/author_index_spec.rb
+++ spec/features/author/author_index_spec.rb
@@ -8,4 +8,10 @@ describe "Author Index page", type: :feature do
     expect(page).to have_link 'New', href: new_author_path
     expect(page).to have_text @alan.first_name
   end
+
+  it "should have a link to the authors edit page" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link href: edit_author_path(@alan)
+  end
 end
