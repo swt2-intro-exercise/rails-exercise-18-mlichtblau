45e6b1bdebe8ad33f65cf0e212461c0dc56e0830
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-02 08:50:12 +0100

    Add paper validation

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "Paper Model", type: :model do
 
   it "should require title to be not empty" do
-    @paper = FactoryBot.create :paper, title: ""
+    @paper = FactoryBot.build(:paper, title: "")
     expect(@paper).to_not be_valid
   end
 end
