5d8c0557f3568da0f004a91204b1d3c970f9ab97
Marius Lichtblau <marius.lichtblau@googlemail.com>
2018-11-02 15:32:09 +0100

    Fertig haha

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,6 @@
 class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
-
+  scope :from_year, -> (year) { where("year = ?", year) if year.present? }
   validates :title, presence: true
   validates :venue, presence: true
   validates :year, presence: true, numericality: true
